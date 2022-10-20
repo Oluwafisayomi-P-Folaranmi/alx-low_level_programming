1963c05296c5d1637ed8fb5ec2daea4d45c72681
Oluwafisayomi-P-Folaranmi <folaranmifisayomi@gmail.com>
2022-10-20 10:01:18 +0100

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> this is a program to check upper case.
+ * @c: an integer parameter.
+ * Return: something successful.
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
